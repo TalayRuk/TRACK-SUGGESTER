8763078e93caab018337b9f1a44fceb84b327aa1
talayruk <notejde@gmail.com>
2016-08-12 12:49:35 -0700

    deleted original header

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
   $(".btn-danger").click(function(){
     $(".jumbotron").hide();
-    $(".alert-success").slidein();
-    $(".questions").fadein();
+    $(".alert-success").show();
+    $(".container").fadeIn();
 
   });
+
+  $(".questions form").submit(function(event){
+    var name = $("input#name").val();
+    $(".name").text(name);
+
+
+    $(".answer").show();
+
+    event.preventDefault();
+  });
+
+
+
 
 });
