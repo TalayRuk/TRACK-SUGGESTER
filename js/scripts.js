0943d078bf056c613a5285cbe369c101de578491
talayruk <notejde@gmail.com>
2016-08-12 13:10:27 -0700

    fixing btn -not success

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,20 @@
-$(document).ready(function(){
-  $(".btn-danger").click(function(){
+$(document).ready(function(event){
+  $("#start").click(function(){
     $(".jumbotron").hide();
     $(".alert-success").show();
     $(".container").fadeIn();
 
+    event.preventDefault();
   });
+});
 
-  $(".questions form").submit(function(event){
+  $("#ready").submit(function(event){
+      event.preventDefault();
     var name = $("input#name").val();
     $(".name").text(name);
 
 
     $(".answer").show();
 
-    event.preventDefault();
+
   });
-
-
-
-
-});
