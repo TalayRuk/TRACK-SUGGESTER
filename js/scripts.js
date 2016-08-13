f413425c616dff3c7c4e11e37aecdd5b048366d4
talayruk <notejde@gmail.com>
2016-08-12 19:02:22 -0700

    fixing bug/s

--- js/scripts.js
+++ js/scripts.js
@@ -17,19 +17,26 @@ $(document).ready(function(event){
     $(".name").text(name);
     alert('HELLO');
 
-    $("#ready").click(function(){
-      // event.preventDefault();
+    $("#ready").click(function(event){
+      event.preventDefault();
       var question1 = parseInt($("input:radio[name=question1]:checked").val());
+      alert(question1);
       var question2 = parseInt($("#question2").val());
+      alert(question2);
       var question3 = parseInt($("#question3").val());
+      alert(question3);
       var question4 = parseInt($("#question4").val());
+      alert(question4);
       var question5 = parseInt($("#questions").val());
+      alert(question5);
 
       var result = add(question1, question2, question3, question4, question5)
-      alert('Yep')
+      alert('Yep');
       if (result <= 7) {
+        $("#8-12").hide();
         $("#5-7").show();
       }else if (result > 7 || result <= 12) {
+        $("#5-7").hide();
         $("#8-12").show();
       }
 
