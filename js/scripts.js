07766694e4e5566ec1e8354f4eb25bc928603def
talayruk <notejde@gmail.com>
2016-08-12 17:53:55 -0700

    fixed indentation

--- js/scripts.js
+++ js/scripts.js
@@ -17,21 +17,25 @@ $(document).ready(function(event){
     $(".name").text(name);
     alert('HELLO');
 
-    var question1 = parseIn($("input:radio[name=question1]:checked").val());
-    var question2 = parseIn($("#question2").val());
-    var question3 = parseIn($("#question3").val());
-    var question4 = parseIn($("#question4").val());
-    var question5 = parseIn($("#questions").val());
-
     $("#ready").click(function(event){
       event.preventDefault();
+      var question1 = parseIn($("input:radio[name=question1]:checked").val());
+      var question2 = parseIn($("#question2").val());
+      var question3 = parseIn($("#question3").val());
+      var question4 = parseIn($("#question4").val());
+      var question5 = parseIn($("#questions").val());
 
-      var number1
+      var score = add(question1, question2, question3, question4, question5)
+
+      if (5<score<7) {
+        $("#5-7").show();
+
+      }
+
+
+
 
     });
-
-
-    if ()
 
 
 
