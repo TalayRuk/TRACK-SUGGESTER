a3250fa49264397ea6d46453851639167a796f7d
talayruk <notejde@gmail.com>
2016-08-12 19:39:51 -0700

    got to alert Q5 then stuck

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@ var add = function (question1, question2, question3, question4, question5) {
 };
 
 $(document).ready(function(){
-  $("#start").click(function(event){
+  $("#start").click(function(){
     $(".jumbotron").hide();
     $(".alert-success").show();
     $(".questions").fadeIn();
     $(".final").hide();
 
-  event.preventDefault();  
+
   });
 
 
@@ -19,33 +19,32 @@ $(document).ready(function(){
     $(".name").text(name);
     alert('HELLO');
 
-    $("#ready").click(function() {
-        var question1 = parseInt($("input:radio[name=question1]:checked").val());
-        alert(question1);
-        var question2 = parseInt($("#question2").val());
-        alert(question2);
-        var question3 = parseInt($("#question3").val());
-        alert(question3);
-        var question4 = parseInt($("#question4").val());
-        alert(question4);
-        var question5 = parseInt($("#question5").val());
-        alert(question5);
+  });
 
-        var result = add(question1, question2, question3, question4, question5)
-        alert(result);
-        if (result <= 7) {
-          $("#8-12").hide();
-          $("#5-7").show();
-        }else if (result > 7 || result <= 12) {
-          $("#5-7").hide();
-          $("#8-12").show();
-        }
+  $("#ready").click(function(event) {
+      var question1 = parseInt($("input:radio[name=question1]:checked").val());
+      alert(question1);
+      var question2 = parseInt($("#question2").val());
+      alert(question2);
+      var question3 = parseInt($("#question3").val());
+      alert(question3);
+      var question4 = parseInt($("#question4").val());
+      alert(question4);
+      var question5 = parseInt($("#question5").val());
+      alert(question5);
 
+      var result = add(question1, question2, question3, question4, question5)
+      alert(result);
+      if (result <= 7) {
+        $("#8-12").hide();
+        $("#5-7").show();
+      }else if (result > 7 || result <= 12) {
+        $("#5-7").hide();
+        $("#8-12").show();
+      }
 
-      });
-
-    });
-
+      event.preventDefault();
+  });
 
 
 
