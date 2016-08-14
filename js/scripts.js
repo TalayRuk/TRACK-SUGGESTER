0c4ab891cd70bbc56a65daef0b41eccd4a459971
Talayruk <notejde@gmail.com>
2016-08-14 10:56:30 -0700

    changed name to .person & #person alert hello not working

--- js/scripts.js
+++ js/scripts.js
@@ -11,47 +11,46 @@ $(document).ready(function(){
 
 
   $(".questions form").submit(function(event){
-    event.preventDefault();
-    var name = $("input#name").val().toUpperCase();
-    $("#name").text(name);
-    alert("Hello, "+ name + " !");
+    var nameInput = $("input#person").val().toUpperCase();
+    $(".person").text(nameInput);
+    alert("Hello, "+ nameInput + " !");
   });
 
   $("#ready").click(function() {
-      var question1 = parseInt($("input:radio[name=question1]:checked").val());
+    var question1 = parseInt($("input:radio[name=question1]:checked").val());
 
-      var question2 = parseInt($("#question2").val());
+    var question2 = parseInt($("#question2").val());
 
-      var question3 = parseInt($("#question3").val());
+    var question3 = parseInt($("#question3").val());
 
-      var question4 = parseInt($("#question4").val());
+    var question4 = parseInt($("#question4").val());
 
-      var question5 = parseInt($("#question5").val());
+    var question5 = parseInt($("#question5").val());
 
-      var result = add(question1, question2, question3, question4, question5);
-      alert(result);
+    var result = add(question1, question2, question3, question4, question5);
 
-      // when I have (13 <= result >= 15) didn't work :(
-      // also using || instead of && also not working
-      // only the the first 2 works!!!! Y?
-      if (result <= 8) {
-        $("#9-12, #13-15, #16-19").hide();
-        $("#8").show();
-      } else if (result >= 9 && result <= 12) {
-        $("#8, #13-15, #16-19").hide();
-        $("#9-12").show();
-      } else if (result >= 13 && result <= 15) {
-        $("#8, #9-12, #16-19").hide();
-        $("#13-15").show();
-      } else {
-        $("#8, #9-12, #13-15").hide();
-        $("#16-19").show();
-      }
+    // when I have (13 <= result >= 15) didn't work :(
+    // also using || instead of && also not working
+    // only the the first 2 works!!!! Y?
+    if (result <= 8) {
+      $("#9-12, #13-15, #16-19").hide();
+      $("#8").show();
+    } else if (result >= 9 && result <= 12) {
+      $("#8, #13-15, #16-19").hide();
+      $("#9-12").show();
+    } else if (result >= 13 && result <= 15) {
+      $("#8, #9-12, #16-19").hide();
+      $("#13-15").show();
+    } else {
+      $("#8, #9-12, #13-15").hide();
+      $("#16-19").show();
+    }
 
     $(".final").show();
     $("#p-l-comic").fadeIn(5000);
     $(".alert-success").hide();
     $(".questions").hide();
 
+    event.preventDefault();
   });
 });
