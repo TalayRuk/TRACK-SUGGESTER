0cf7170ced35c45e693f7b0ed3b27ac57535f853
Talayruk <notejde@gmail.com>
2016-08-13 20:58:59 -0700

    commented out event still not working

--- js/scripts.js
+++ js/scripts.js
@@ -14,23 +14,23 @@ $(document).ready(function(){
 
     var name = $("input#name").val().toUpperCase();
     $(".name").text(name);
-    alert('HELLO');
+    alert('HELLO, '+ name);
   });
 
-  $("#ready").click(function(event) {
+  $("#ready").click(function() {
       var question1 = parseInt($("input:radio[name=question1]:checked").val());
-      
+
       var question2 = parseInt($("#question2").val());
-      
+
       var question3 = parseInt($("#question3").val());
-      
+
       var question4 = parseInt($("#question4").val());
-      
+
       var question5 = parseInt($("#question5").val());
-      
+
       var result = add(question1, question2, question3, question4, question5);
-      
-      
+
+
       if (result <= 7) {
         $("#8-12, #13-15, #16-19").hide();
         $("#5-7").show();
@@ -40,15 +40,15 @@ $(document).ready(function(){
       }else if (result > 12 || result <= 15) {
         $("#5-7, #8-12, #16-19").hide();
         $("#13-15").show();
-      }else (result > 15 || result <= 19) {
+      }else if (result > 15 || result <= 19) {
         $("#5-7, #8-12, #13-15").hide();
         $("#16-19").show();
-      } 
+      }
 
     $(".final").show();
     $(".alert-success").hide();
     $(".questions").fadeOut();
-    
-    event.preventDefault();
+
+    // event.preventDefault();
   });
 });
